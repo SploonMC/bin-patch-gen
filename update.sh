f0ce8afbd90addbba2607b868ae252756dc8e9de
Nils Krüger <radsteve@radsteve.net>
2024-11-16 10:19:33 +0100

    ci: improve update script for git

--- update.sh
+++ update.sh
@@ -6,33 +6,31 @@ git checkout release
 git branch --set-upstream-to=origin/release release
 git pull
 
+if [[ ! -d "work/.git" ]]; then
+  mkdir -p work
+  cd work
+  chown -R radsteve .
+  git init .
+  git remote add origin https://github.com/SploonMC/patches.git
+  git pull
+fi
+
 # check if docker container exists
 if docker container inspect sploon-bin-patch-gen > /dev/null 2>&1; then
   # if it does, check if it's running
   if [ "$(docker container inspect -f '{{.State.Status}}' sploon-bin-patch-gen)" != "running" ]; then
-    # restart docker container, this will block until finished
     docker compose down
     docker compose up
 
-    # git shit
     cd work || exit 1
-    [[ ! -d ".git" ]] && git init .
-    git remote remove origin
-    git remote add origin https://github.com/SploonMC/patches.git
     git add .
     git commit -am "automated update"
     git push -u origin master
   fi
 else
-  # If the container doesn't exist, assume first time setup
-  echo "Container does not exist, assuming this is the first time."
   docker compose up
 
-  # git shit for first-time setup
   cd work || exit 1
-  [[ ! -d ".git" ]] && git init .
-  git remote remove origin
-  git remote add origin https://github.com/SploonMC/patches.git
   git add .
   git commit -am "first-time setup"
   git push -u origin master
