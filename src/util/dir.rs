116fa1f930e4032338a5205811dd04e2c57ba9b5
Nils Krüger <radsteve@radsteve.net>
2024-11-18 16:35:00 +0100

    feat: library meta

--- src/util/dir.rs
+++ src/util/dir.rs
@@ -65,7 +65,7 @@ pub async fn clear_directory<P: AsRef<Path>>(path: P) -> io::Result<()> {
 fn clean_temp_dirs() {
     unsafe {
         for dir in TEMP_DIRS.clone() {
-            let _ = fs::remove_dir_all(dir);
+            //let _ = fs::remove_dir_all(dir);
         }
     }
 }
